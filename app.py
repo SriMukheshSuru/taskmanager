1af67b1454c1c5013ab57f87e7cf5a11f08a9c33
SriMukheshSuru <21ee01025@iitbbs.ac.in>
2025-05-29 17:59:48 +0530

    ui update

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask,render_template,url_for,request,redirect
 
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import pytz
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
@@ -12,7 +13,8 @@ class Todo(db.Model):
     id = db.Column(db.Integer,primary_key=True)
     content = db.Column(db.String(200),nullable = False)
     completed = db.Column(db.Integer,default = 0)
-    date_created = db.Column(db.DateTime,default= datetime.utcnow)
+    # date_created = db.Column(db.DateTime,default= datetime.utcnow)
+    date_created = db.Column(db.DateTime, default=lambda: datetime.now(pytz.timezone('Asia/Kolkata')))
 
     def __repr__(self):
         return '<Task %r>' % self.id
